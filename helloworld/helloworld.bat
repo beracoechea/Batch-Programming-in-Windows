b6f16d186bad4e5ac57a8e95166234410e347638
beracoechea <113546739+beracoechea@users.noreply.github.com>
2024-02-21 23:43:18 -0600

    Add files via upload

---
+++ helloworld/helloworld.bat
@@ -0,0 +1,14 @@
+@echo on
+echo Hello World
+echo.
+echo Listado del directorio actual:
+dir
+echo.
+echo Creando el subdirectorio Test...
+mkdir Test
+echo.
+echo Cambiándose al subdirectorio Test...
+cd Test
+echo.
+echo Listado del contenido del subdirectorio Test:
+dir
